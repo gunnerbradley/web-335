34cc7759f2b39265268a07b555139428dc61faf5
Gunner Bradley <gunnerbradley.dev@gmail.com>
2021-12-12 21:37:01 -0600

    8.3

---
+++ week-8/bradley_calculator.py
@@ -0,0 +1,19 @@
+# ============================================
+# ; Title:  bradley_calculator.py
+# ; Author: Gunner Bradley
+# ; Date:   12 December 2021
+# ; Description: Basic demo of Python functions
+# ;===========================================
+
+def add(argument1, argument2):
+    return argument1 + argument2
+
+def subtract(argument1, argument2):
+    return argument1 - argument2
+
+def divide(argument1, argument2):
+    return argument1 / argument2
+
+print(add(1, 2))
+print(subtract(4, 1))
+print(divide(8, 2))
--- week-8/bradley_hello_world.py
+++ week-8/bradley_hello_world.py
@@ -1,1 +1,7 @@
+# ============================================
+# ; Title:  bradley_hello_world.py
+# ; Author: Gunner Bradley
+# ; Date:   12 December 2021
+# ; Description: Basic demo of Python Print method
+# ;===========================================
 print('Hello World')
